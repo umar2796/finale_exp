936de1f247202eb741b5ae722a425fca43ffe32c
Umar <umarrafikshaikh@gmail.com>
2022-05-18 23:21:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const app=express();
+const port=4000;
+
+app.use(express.static("public"));
+app.listen(port,()=>{
+    console.log(`app is started at ${port}`);       //` is used for string formating
+})
+
